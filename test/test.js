927992bc33494000d764a8ac47cdf255e622730e
Shahzad Latif <shahzad.latif@aspose.com>
2015-11-06 11:01:13 +0500

    Add node app

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var request = require('supertest');
+
+var app = require('../app.js');
+
+describe('GET /', function() {
+	it('respond with Hello World!', function(done) {
+		request(app).get('/').expect('Hello World!', done);
+	});
+});
